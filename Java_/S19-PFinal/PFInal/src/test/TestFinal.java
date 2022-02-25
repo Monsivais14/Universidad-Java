2cc7f70784c7f2a44b0919d0cc1c581df0cfe0c0
Alfredo Monsivais <mpop@pop-os.localdomain>
2022-02-24 21:45:03 -0600

    v0.4.5

--- Java_/S19-PFinal/PFInal/src/test/TestFinal.java
+++ Java_/S19-PFinal/PFInal/src/test/TestFinal.java
@@ -1,5 +1,7 @@
 package test;
 
+import domain.Persona;
+
 public class TestFinal {
 
     public static void main(String[] args) {
@@ -9,6 +11,19 @@ public class TestFinal {
         System.out.println("n1 = " + n1);
 
         System.out.println(domain.Persona.CONSTANTE);
+
+        final Persona p1 = new Persona();
+        //p1 = new Persona();
+        p1.setNombre("Juan");
+        System.out.println(p1.getNombre());
+
+        Persona p2 = new Persona();
+        p2 = new Persona();//cambio de referencia a otor objeto
+        /*
+        Final en objetos hace referencia a que no se puede cambiar el apuntador
+        que tiene la variable hacia otro objeto en memoria heap, solo modificar
+        sus atributos del mismo
+         */
 
     }
 }
