c0032a4bb54d2d2f500016572bd649a20824c453
Monsivais14 <monsivaisalfredo2@gmail.com>
2022-04-04 22:38:02 -0500

    v0.7

---
+++ Java_/Java_JDBC/S45-ManejoJDBC/ManejoUsuarios-Lab/ManejoUsuarios/src/main/java/com/users/datos/DAO.java
@@ -0,0 +1,12 @@
+
+package com.users.datos;
+
+/**
+ *
+ * @author mpop
+ */
+public interface DAO {
+    
+    
+    
+}
---
+++ Java_/Java_JDBC/S45-ManejoJDBC/ManejoUsuarios-Lab/ManejoUsuarios/src/main/java/com/users/domain/User.java
@@ -0,0 +1,17 @@
+
+package com.users.domain;
+
+/**
+ *
+ * @author mpop
+ */
+public class User {
+    
+    //atributos de un objeto usuario
+    Integer idUsuario;
+    String name;
+    String password;
+    
+    
+    
+}
